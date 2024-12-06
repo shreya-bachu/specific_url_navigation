657bf3eed1381420b9889f6fbf32596d07f62487
shreya-bachu <bachushreya7@gmail.com>
2024-12-06 11:59:09 +0530

    first commit

---
+++ food/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+def veg(request):
+    return render(request,'veg.html')
+
+# Create your views here.
---
+++ shopping/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+def dress(request):
+    return render(request,'dress.html')
+
+# Create your views here.
